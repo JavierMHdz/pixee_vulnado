15a52f97d0e1903d773e4060eb0f54d1457941e7
Javier Macossay <macossay@usc.edu>
2024-01-16 16:53:45 -0600

    Create HelloWorld.java
    @pixeebot next

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+// Your First Program
+
+class HelloWorld {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int c = ((a+b));
+        System.out.println(("Hello World!")); 
+    }
+}
